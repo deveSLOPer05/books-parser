838d068b14e9503fb8d9d30b8473cbffaba83f03
devrSLOPer05 <daria.levoshina@mail.ru>
2026-09-03 20:13:26 +0300

    парсер успешно собирает название, автора и цену

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import pandas as pd
+#import pandas as pd
 
 url = "https://www.chitai-gorod.ru/catalog/books-18030"
 headers = {
@@ -9,5 +9,14 @@ headers = {
     "Referer": "https://google.com" #header взят рандомный из гугла целиком
 }
 r = requests.get(url, headers=headers)
-print(r.status_code)
-print(r.text)
+#print(r.status_code)
+#print(r.text)
+
+soup = bs(r.text, "html.parser")
+books_name = soup.find_all("a", class_="product-card__title")
+books_price = soup.find_all("span", class_="product-mini-card-price__price product-mini-card-price__price--reverse")
+for name in books_name:
+    print(name["title"])
+    title = name.text.strip()
+for price in books_price:
+    print(price.text)
